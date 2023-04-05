e14577e3885ceb37ce8b7fc3c0f873a482655b46
Generekalipsa <djgamez1222@gmail.com>
2023-04-05 21:17:34 +0200

    branch5

--- skrypt.sh
+++ skrypt.sh
@@ -14,13 +14,22 @@ while test $# -gt 0; do
 					do 
 						echo "log$x.txt $script_name $file_date" > log$x.txt
 					done
+					shift
 			else
 				for x in {1..100}
 					do 
 						echo "log$x.txt $script_name $file_date" > log$x.txt
 					done
+					shift
 			fi
+			;;
+		--help)
+			echo "Usage: ./skrypt.sh [OPTION]
+--date					Show date
+--logs <LOG_NUMBER>			Create logs (default 100)
+--help					Show this help message"
 			exit 0
+			shift
 			;;
 		*)
 			echo "$1 flag does not exist"
