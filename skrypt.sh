82ca2157b501ca36aa84931b502e42f9954e9add
Generekalipsa <djgamez1222@gmail.com>
2023-04-05 20:26:36 +0200

    branch3 commit

--- skrypt.sh
+++ skrypt.sh
@@ -6,6 +6,15 @@ while test $# -gt 0; do
 			date
 			shift
 			;;
+		--logs)		
+			script_name=$(basename $0)
+			file_date=$(date)
+			for x in {1..100}
+				do 
+					echo "log$x.txt $script_name $file_date" > log$x.txt
+				done
+			shift
+			;;
 		*)
 			echo "$1 flag does not exist"
 			return 1;
